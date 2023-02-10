6bbf7047fc07633d6e75326023f5c779ca161240
abdullah-dev5 <113471263+abdullah-dev5@users.noreply.github.com>
2023-02-10 19:32:31 +0500

    changed

--- Lab 02 java/Task3.java
+++
@@ -1,9 +0,0 @@
-public class Task3 {
-    public static void main(String[] args)
-    {
-        String change="The quick brown fox jumps over the lazy dog.";
-        System.out.println(change.replace("fox", "cat"));
-
-    }
-
-}
