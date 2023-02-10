d7902661394cdf58dc3c3ad6f1e1d8c1b8d80955
abdullah-dev5 <113471263+abdullah-dev5@users.noreply.github.com>
2023-02-10 19:38:17 +0500

    Replace word in String
    Using replace method
    replace any word or alphabet ..
    replace(old word, new word)

---
+++ Lab 02 java/Task3.java
@@ -0,0 +1,9 @@
+public class Task3 {
+    public static void main(String[] args)
+    {
+        String change="The quick brown fox jumps over the lazy dog.";
+        System.out.println(change.replace("fox", "cat"));
+
+    }
+
+}
