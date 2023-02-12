058cb5e8e847738bb9e05d18a98fe9b6db88d8bb
abdullah-dev5 <113471263+abdullah-dev5@users.noreply.github.com>
2023-02-12 19:52:40 +0500

    Multi Line Print Statement in java
    we  can write our print Statement on more than one line .
    we don't need Semi-colon .

---
+++ Concept In java/MulitLinePrintStatement.java
@@ -0,0 +1,13 @@
+public class MulitLinePrintStatement {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //we can also write statement in piece
+        // there is no need of Semicolon after the statement
+
+        System.out.println("hello" +
+                " " +
+                "World!");
+
+    }
+
+}
