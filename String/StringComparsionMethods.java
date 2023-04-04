e8cf62f6fd69fa455406e49e8e07593eab76acb9
abdullah-dev5 <113471263+abdullah-dev5@users.noreply.github.com>
2023-04-04 12:29:23 +0500

    Create StringComparsionMethods.java

---
+++ String/StringComparsionMethods.java
@@ -0,0 +1,21 @@
+public class StringComparsionMethods {
+
+    public static void main(String[] args) {
+        String str1="I";
+        str1+="Love itachi";
+
+        String str2="I Love";
+        str2+=" itachi";
+
+        //if(str1.equals(str2))
+        if(str1==str2)
+        {
+            System.out.println("str1 and str2 are equal");
+        }
+        else {
+            System.out.println("str1 and str2 are not equal");
+        }
+        //when we excute the result will not be equal
+        
+    }
+}
