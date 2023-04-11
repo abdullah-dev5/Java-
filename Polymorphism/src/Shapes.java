f4c3fcbd922419b0c211e7d9d82f523484ea7051
abdullah-dev5 <113471263+abdullah-dev5@users.noreply.github.com>
2023-04-11 21:59:47 +0500

    Create Shapes.java

---
+++ Polymorphism/src/Shapes.java
@@ -0,0 +1,15 @@
+ class Shapes {
+    int width;
+    int height;
+
+    static void greetings()
+    {
+        System.out.println("Hello , this is in Shape Class ");
+    }
+
+    void area ()
+    {
+        System.out.println("Im in Shapes");
+    }
+
+}
